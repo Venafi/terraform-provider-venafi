74026893197bde1e8e596e3c15c2678387710361
Peter Fiddes <peter.fiddes@venafi.com>
2024-10-29 15:54:26 +0000

    deps: Remove fix covered in #152 to avoid conflict

--- venafi/provider.go
+++ venafi/provider.go
@@ -10,10 +10,11 @@ import (
 	"os"
 	"strings"
 
+	"golang.org/x/crypto/pkcs12"
+
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"software.sslmate.com/src/go-pkcs12"
 
 	"github.com/Venafi/vcert/v5"
 	"github.com/Venafi/vcert/v5/pkg/endpoint"
