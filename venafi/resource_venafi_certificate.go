70a7bf05e857ed5cc847fd91e7cef9f4ca5ba69c
Jiang Gao <59629351+jiang-gao@users.noreply.github.com>
2025-07-26 23:16:49 -0400

    Add WriteOnly: true to key_password schema to prevent sensitive value from leaking in state

--- venafi/resource_venafi_certificate.go
+++ venafi/resource_venafi_certificate.go
@@ -159,6 +159,7 @@ func resourceVenafiCertificate() *schema.Resource {
 				ForceNew:    true,
 				Description: "Password for the private key",
 				Sensitive:   true,
+                                WriteOnly:   true,
 			},
 			"expiration_window": {
 				Type:        schema.TypeInt,
